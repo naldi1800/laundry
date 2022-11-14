cfcb691eb4fcb4e93b2be0ea50b029584e656033
naldi1800 <76877029+naldi1800@users.noreply.github.com>
2022-11-15 07:47:29 +0800

    Update config.php

--- App/Controller/config.php
+++ App/Controller/config.php
@@ -3,8 +3,8 @@
 
     define("DB_HOST", "localhost");
     define("DB_USER", "root");
-    define("DB_PASS", "root1234");
-    define("DB_NAME", "loundry");
+    define("DB_PASS", "");
+    define("DB_NAME", "laundry");
 
     $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
